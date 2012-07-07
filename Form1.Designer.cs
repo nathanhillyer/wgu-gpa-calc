30acca7a2e4d3f5799bb39c4b3da4858bb704eb8
Nathan Hillyer <nathan@hillyer.me>
2012-07-07 00:22:17 -0500

    alpha version 0.1.0
    first day of coding

---
+++ Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace wgu_gpa_calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClassDescription = new System.Windows.Forms.ToolTip(this.components);
+            this.lblWFV1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel7 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel8 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel9 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel10 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel11 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel12 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel13 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel14 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel15 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel16 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel17 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel18 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel19 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel20 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel21 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel22 = new System.Windows.Forms.LinkLabel();
+            this.txtWFV1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblWFV1val = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWFV1
+            // 
+            this.lblWFV1.AutoSize = true;
+            this.lblWFV1.Location = new System.Drawing.Point(12, 9);
+            this.lblWFV1.Name = "lblWFV1";
+            this.lblWFV1.Size = new System.Drawing.Size(40, 13);
+            this.lblWFV1.TabIndex = 2;
+            this.lblWFV1.TabStop = true;
+            this.lblWFV1.Text = "WFV1:";
+            this.ClassDescription.SetToolTip(this.lblWFV1, "IT Fundamentals I (CIW 1D0-510 Web Foundations Associate)");
+            this.lblWFV1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblWFV1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(12, 40);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel2.TabIndex = 2;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "WFV1:";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(12, 71);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel3.TabIndex = 2;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "WFV1:";
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Location = new System.Drawing.Point(13, 102);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel4.TabIndex = 2;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "WFV1:";
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Location = new System.Drawing.Point(12, 133);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel5.TabIndex = 2;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "WFV1:";
+            // 
+            // linkLabel6
+            // 
+            this.linkLabel6.AutoSize = true;
+            this.linkLabel6.Location = new System.Drawing.Point(13, 164);
+            this.linkLabel6.Name = "linkLabel6";
+            this.linkLabel6.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel6.TabIndex = 2;
+            this.linkLabel6.TabStop = true;
+            this.linkLabel6.Text = "WFV1:";
+            // 
+            // linkLabel7
+            // 
+            this.linkLabel7.AutoSize = true;
+            this.linkLabel7.Location = new System.Drawing.Point(13, 195);
+            this.linkLabel7.Name = "linkLabel7";
+            this.linkLabel7.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel7.TabIndex = 2;
+            this.linkLabel7.TabStop = true;
+            this.linkLabel7.Text = "WFV1:";
+            // 
+            // linkLabel8
+            // 
+            this.linkLabel8.AutoSize = true;
+            this.linkLabel8.Location = new System.Drawing.Point(12, 226);
+            this.linkLabel8.Name = "linkLabel8";
+            this.linkLabel8.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel8.TabIndex = 2;
+            this.linkLabel8.TabStop = true;
+            this.linkLabel8.Text = "WFV1:";
+            // 
+            // linkLabel9
+            // 
+            this.linkLabel9.AutoSize = true;
+            this.linkLabel9.Location = new System.Drawing.Point(12, 257);
+            this.linkLabel9.Name = "linkLabel9";
+            this.linkLabel9.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel9.TabIndex = 2;
+            this.linkLabel9.TabStop = true;
+            this.linkLabel9.Text = "WFV1:";
+            // 
+            // linkLabel10
+            // 
+            this.linkLabel10.AutoSize = true;
+            this.linkLabel10.Location = new System.Drawing.Point(12, 288);
+            this.linkLabel10.Name = "linkLabel10";
+            this.linkLabel10.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel10.TabIndex = 2;
+            this.linkLabel10.TabStop = true;
+            this.linkLabel10.Text = "WFV1:";
+            // 
+            // linkLabel11
+            // 
+            this.linkLabel11.AutoSize = true;
+            this.linkLabel11.Location = new System.Drawing.Point(12, 319);
+            this.linkLabel11.Name = "linkLabel11";
+            this.linkLabel11.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel11.TabIndex = 2;
+            this.linkLabel11.TabStop = true;
+            this.linkLabel11.Text = "WFV1:";
+            // 
+            // linkLabel12
+            // 
+            this.linkLabel12.AutoSize = true;
+            this.linkLabel12.Location = new System.Drawing.Point(12, 350);
+            this.linkLabel12.Name = "linkLabel12";
+            this.linkLabel12.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel12.TabIndex = 2;
+            this.linkLabel12.TabStop = true;
+            this.linkLabel12.Text = "WFV1:";
+            // 
+            // linkLabel13
+            // 
+            this.linkLabel13.AutoSize = true;
+            this.linkLabel13.Location = new System.Drawing.Point(12, 381);
+            this.linkLabel13.Name = "linkLabel13";
+            this.linkLabel13.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel13.TabIndex = 2;
+            this.linkLabel13.TabStop = true;
+            this.linkLabel13.Text = "WFV1:";
+            // 
+            // linkLabel14
+            // 
+            this.linkLabel14.AutoSize = true;
+            this.linkLabel14.Location = new System.Drawing.Point(12, 412);
+            this.linkLabel14.Name = "linkLabel14";
+            this.linkLabel14.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel14.TabIndex = 2;
+            this.linkLabel14.TabStop = true;
+            this.linkLabel14.Text = "WFV1:";
+            // 
+            // linkLabel15
+            // 
+            this.linkLabel15.AutoSize = true;
+            this.linkLabel15.Location = new System.Drawing.Point(13, 443);
+            this.linkLabel15.Name = "linkLabel15";
+            this.linkLabel15.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel15.TabIndex = 2;
+            this.linkLabel15.TabStop = true;
+            this.linkLabel15.Text = "WFV1:";
+            // 
+            // linkLabel16
+            // 
+            this.linkLabel16.AutoSize = true;
+            this.linkLabel16.Location = new System.Drawing.Point(12, 474);
+            this.linkLabel16.Name = "linkLabel16";
+            this.linkLabel16.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel16.TabIndex = 2;
+            this.linkLabel16.TabStop = true;
+            this.linkLabel16.Text = "WFV1:";
+            // 
+            // linkLabel17
+            // 
+            this.linkLabel17.AutoSize = true;
+            this.linkLabel17.Location = new System.Drawing.Point(13, 505);
+            this.linkLabel17.Name = "linkLabel17";
+            this.linkLabel17.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel17.TabIndex = 2;
+            this.linkLabel17.TabStop = true;
+            this.linkLabel17.Text = "WFV1:";
+            // 
+            // linkLabel18
+            // 
+            this.linkLabel18.AutoSize = true;
+            this.linkLabel18.Location = new System.Drawing.Point(13, 536);
+            this.linkLabel18.Name = "linkLabel18";
+            this.linkLabel18.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel18.TabIndex = 2;
+            this.linkLabel18.TabStop = true;
+            this.linkLabel18.Text = "WFV1:";
+            // 
+            // linkLabel19
+            // 
+            this.linkLabel19.AutoSize = true;
+            this.linkLabel19.Location = new System.Drawing.Point(12, 567);
+            this.linkLabel19.Name = "linkLabel19";
+            this.linkLabel19.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel19.TabIndex = 2;
+            this.linkLabel19.TabStop = true;
+            this.linkLabel19.Text = "WFV1:";
+            // 
+            // linkLabel20
+            // 
+            this.linkLabel20.AutoSize = true;
+            this.linkLabel20.Location = new System.Drawing.Point(12, 598);
+            this.linkLabel20.Name = "linkLabel20";
+            this.linkLabel20.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel20.TabIndex = 2;
+            this.linkLabel20.TabStop = true;
+            this.linkLabel20.Text = "WFV1:";
+            // 
+            // linkLabel21
+            // 
+            this.linkLabel21.AutoSize = true;
+            this.linkLabel21.Location = new System.Drawing.Point(12, 629);
+            this.linkLabel21.Name = "linkLabel21";
+            this.linkLabel21.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel21.TabIndex = 2;
+            this.linkLabel21.TabStop = true;
+            this.linkLabel21.Text = "WFV1:";
+            // 
+            // linkLabel22
+            // 
+            this.linkLabel22.AutoSize = true;
+            this.linkLabel22.Location = new System.Drawing.Point(12, 660);
+            this.linkLabel22.Name = "linkLabel22";
+            this.linkLabel22.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel22.TabIndex = 2;
+            this.linkLabel22.TabStop = true;
+            this.linkLabel22.Text = "WFV1:";
+            // 
+            // txtWFV1
+            // 
+            this.txtWFV1.Location = new System.Drawing.Point(59, 9);
+            this.txtWFV1.Name = "txtWFV1";
+            this.txtWFV1.Size = new System.Drawing.Size(100, 20);
+            this.txtWFV1.TabIndex = 3;
+            this.txtWFV1.TextChanged += new System.EventHandler(this.txtWFV1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(642, 373);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(636, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate GPA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblWFV1val
+            // 
+            this.lblWFV1val.AutoSize = true;
+            this.lblWFV1val.Location = new System.Drawing.Point(166, 9);
+            this.lblWFV1val.Name = "lblWFV1val";
+            this.lblWFV1val.Size = new System.Drawing.Size(0, 13);
+            this.lblWFV1val.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 708);
+            this.Controls.Add(this.lblWFV1val);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtWFV1);
+            this.Controls.Add(this.linkLabel22);
+            this.Controls.Add(this.linkLabel11);
+            this.Controls.Add(this.linkLabel21);
+            this.Controls.Add(this.linkLabel10);
+            this.Controls.Add(this.linkLabel20);
+            this.Controls.Add(this.linkLabel9);
+            this.Controls.Add(this.linkLabel19);
+            this.Controls.Add(this.linkLabel18);
+            this.Controls.Add(this.linkLabel8);
+            this.Controls.Add(this.linkLabel17);
+            this.Controls.Add(this.linkLabel7);
+            this.Controls.Add(this.linkLabel16);
+            this.Controls.Add(this.linkLabel6);
+            this.Controls.Add(this.linkLabel15);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel14);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.linkLabel13);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel12);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.lblWFV1);
+            this.Name = "Form1";
+            this.Text = "FOOB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip ClassDescription;
+        private System.Windows.Forms.LinkLabel lblWFV1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.LinkLabel linkLabel6;
+        private System.Windows.Forms.LinkLabel linkLabel7;
+        private System.Windows.Forms.LinkLabel linkLabel8;
+        private System.Windows.Forms.LinkLabel linkLabel9;
+        private System.Windows.Forms.LinkLabel linkLabel10;
+        private System.Windows.Forms.LinkLabel linkLabel11;
+        private System.Windows.Forms.LinkLabel linkLabel12;
+        private System.Windows.Forms.LinkLabel linkLabel13;
+        private System.Windows.Forms.LinkLabel linkLabel14;
+        private System.Windows.Forms.LinkLabel linkLabel15;
+        private System.Windows.Forms.LinkLabel linkLabel16;
+        private System.Windows.Forms.LinkLabel linkLabel17;
+        private System.Windows.Forms.LinkLabel linkLabel18;
+        private System.Windows.Forms.LinkLabel linkLabel19;
+        private System.Windows.Forms.LinkLabel linkLabel20;
+        private System.Windows.Forms.LinkLabel linkLabel21;
+        private System.Windows.Forms.LinkLabel linkLabel22;
+        private System.Windows.Forms.TextBox txtWFV1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblWFV1val;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace wgu_gpa_calc
+{
+    public partial class Form1 : Form
+    {
+        private const double basePercent = 85;
+        // passing score and maximum score for WFV1 class
+        private const int WFV1minScore = 54;
+        private const int WFV1maxScore = 85;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblWFV1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // loads CIW website for description of certification
+            VisitLink("http://www.ciwcertified.com/certifications/web_foundations_series/associate.php", lblWFV1);
+        }
+
+        // loads a link when label obj is clicked
+        private void VisitLink(String url, LinkLabel obj)
+        {
+            try
+            {
+                // opens up link in default web browser
+                System.Diagnostics.Process.Start(url);
+                obj.LinkVisited = true;
+            }
+            catch
+            {
+                // displays error message if link doesn't open
+                MessageBox.Show("Unable to open link from a partial-trust environment (such as a shared drive).");
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double WFV1Percent = 80;
+
+            try
+            {
+                WFV1Percent = GetPercent(WFV1minScore, WFV1maxScore, txtWFV1);
+            }
+            catch
+            {
+                return;
+            }
+            
+            double gpa = PercentToGPA(WFV1Percent);
+            
+            textBox1.Text = "GPA = " + gpa;
+        }
+
+        private void txtWFV1_TextChanged(object sender, EventArgs e)
+        {
+            // clear label of previous validation message
+            lblWFV1val.Text = "";
+
+            // validates entry if user has entered anything
+            if (txtWFV1.Text.Length > 0)
+            {
+                Validate(WFV1minScore, WFV1maxScore);
+            }
+        }
+
+        private double GetPercent(int minScore, int maxScore, TextBox txt)
+        {
+            int score = 0;
+
+            // attempts to parse an integer from the text entered in WFV1 score text box
+            // shows error message if fails
+            try
+            {
+                score = int.Parse(txt.Text);
+
+                // subtracts score from the minimum score for gpa calculation
+                score = score - minScore;
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a numeric value for WFV1.");
+                throw;
+            }
+            // calculates how many percentage points to add to the base percentage
+            double percentToAdd = (double)score / (maxScore - minScore) * (100 - basePercent);
+            double finalPercent = percentToAdd + basePercent;
+            return finalPercent;
+        }
+
+        private double PercentToGPA(double percent)
+        {
+            double gpa = 3.0;
+
+            if (percent >= 93)
+                gpa = 4.0;
+            else if (percent >= 90)
+                gpa = 3.67;
+            else if (percent >= 87)
+                gpa = 3.33;
+
+            return gpa;
+        }
+
+        private void Validate(int minScore, int maxScore)
+        {
+            // number to be parsed from WFV1 text box
+            int number;
+
+            // if parse fails
+            if (!int.TryParse(txtWFV1.Text, out number))
+            {
+                // validation message will appear
+                lblWFV1val.Text = "Please enter a numeric value.";
+            }
+            else
+            {
+                if (number < minScore)
+                {
+                    lblWFV1val.Text = "Please enter a passing score.";
+                }
+                if (number > maxScore)
+                {
+                    lblWFV1val.Text = "Please enter a score less than or equal to 85.";
+                }
+            }
+        }
+    }
+}
